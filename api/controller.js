d11dd8a7fb805bf1cb633269a162e7273c980599
happpy <alonescar.dhl@gmail.com>
2023-09-03 22:29:30 +0800

    update api error handle

--- api/controller.js
+++ api/controller.js
@@ -16,7 +16,7 @@ export const getAllMemos = async (_, res) => {
     .then((memos) => res.json(memos))
     .catch((err) => {
       console.log(err);
-      res.status(500);
+      res.status(500).send("Server error");
     });
 };
 
@@ -28,14 +28,14 @@ export const deleteAllMemos = async (_, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500);
+      res.status(500).send("Server error");
     });
 };
 
 export const addMemo = async (req, res) => {
   const { texts } = req.body;
   if (vertifyTexts(texts)) {
-    return res.status(400);
+    return res.status(400).send("Server error");
   }
   const MemoDoc = new MemoModel({ texts });
   await MemoDoc.save()
@@ -44,7 +44,7 @@ export const addMemo = async (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500);
+      res.status(500).send("Server error");
     });
 };
 
@@ -52,7 +52,7 @@ export const updateMemo = async (req, res) => {
   const { texts } = req.body;
   const update_time = Date.now();
   if (vertifyTexts(texts)) {
-    return res.status(400);
+    return res.status(400).send("Server error");
   }
   await MemoModel.updateOne(
     { _id: req.params.id },
@@ -67,7 +67,7 @@ export const updateMemo = async (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(400);
+      res.status(400).send("Server error");
     });
 };
 
@@ -78,7 +78,7 @@ export const deleteMemo = async (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500);
+      res.status(500).send("Server error");
     });
 };
 
@@ -87,6 +87,6 @@ export const getMemo = async (req, res) => {
     .then((memo) => res.json(memo))
     .catch((err) => {
       console.log(err);
-      res.status(500);
+      res.status(500).send("Server error");
     });
 };
